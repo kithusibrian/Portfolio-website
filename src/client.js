ea6884cdd7c479ebd2f2719dbc7332859a9e7681
MUSEMBI BRIAN KITHUSI <112511014+kithusibrian@users.noreply.github.com>
2023-10-10 12:35:46 +0300

    Adding in Sanity Studio

---
+++ src/client.js
@@ -0,0 +1,6 @@
+import { SanityClient } from "@sanity/client";
+
+export default SanityClient({
+  projectId: "10j7vps6",
+  dataset: "production",
+});
---
+++ studio/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: '10j7vps6',
+    dataset: 'production'
+  }
+})
